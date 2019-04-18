245cb1fc00799da7b2dfd24fc570542888570dc1
fxiong <1308694300@qq.com>
2019-04-18 10:09:46 +0800

    vue组件化编码

---
+++ src/App.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="todo-container">
+    <div class="todo-wrap">
+      <Header :addTodo="addTodo"/>
+      <Main :todos="todos" :deleteTodo="deleteTodo"/>
+      <Footer :todos="todos" :slectAllTodos="slectAllTodos" :deleteAllCompleted="deleteAllCompleted" />
+    </div>
+  </div>
+</template>
+<script>
+  // import Header from './components/Header.vue'
+  import Main from './components/Main.vue'
+  import Footer from './components/Footer.vue'
+  import storageUtils from "./utils/storageUtils";
+
+  export default {
+
+    data () {
+      return {
+        todos: storageUtils.getTodos()
+      }
+    },
+
+    methods: {
+      // 添加todo
+      addTodo (todo) {
+        this.todos.unshift(todo)
+      },
+
+      // 删除todo
+      deleteTodo (index) {
+        this.todos.splice(index, 1)
+      },
+
+      // 对所有todo进行全选或全不选
+      slectAllTodos (isCheck) {
+        this.todos.forEach(todo => todo.completed = isCheck)
+      },
+
+      // 删除所有已完成的
+      deleteAllCompleted () {
+        this.todos = this.todos.filter(todo => !todo.completed)
+      },
+
+      selectTodo (todo, isCheck) {
+        todo.completed = isCheck
+      }
+
+
+    },
+    watch:{
+        todos:{
+          deep:true,//深度监视
+          handler:storageUtils.saveTodos
+        }
+    },
+    components: {
+      // Header,
+      Main,
+      Footer
+    }
+  }
+</script>
+<style>
+  .todo-container {
+    width: 600px;
+    margin: 0 auto;
+  }
+
+  .todo-container .todo-wrap {
+    padding: 10px;
+    border: 1px solid #ddd;
+    border-radius: 5px;
+  }
+</style>
---
+++ src/utils/storageUtils.js
@@ -0,0 +1,13 @@
+/*
+* 包含一些local存储的工具函数的模块
+* */
+export default{
+  //读取todos
+  getTodos(){
+    return JSON.parse(localStorage.getItem('todos_key') || '[]')
+  },
+  //保存todos
+  saveTodos(todos){
+    localStorage.setItem('todos_key',JSON.stringify(todos))
+  }
+}
